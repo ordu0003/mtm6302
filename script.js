65f24f3aa8fd64e13af64bf57f4bd42fd6f60ec1
Jessica Orduz <ordu0003@algonquinlive.com>
2023-01-19 15:35:47 -0500

    creats java file

---
+++ script.js
@@ -0,0 +1,57 @@
+console.log("Hello2")
+
+let firstName ="Jane"
+firstName ="Joe"
+const lastName ="Doe"
+
+//concatenation (merge variable)
+console.log ( firstName)
+
+console.log( firstName + lastName + "Ram" )
+
+const temlLiterals = `My first name is: ${firstName}, My lastName: ${lastName}, ${2+2}`
+
+console.log(temlLiterals)
+
+
+ ///arrays
+
+ const simpleList = [
+     "Jess", //0
+     5,//1
+     false,
+     [1, 2, "three"]
+    ]//3
+
+ console.log( simpleList[0] )
+
+ //objects: unordered list> key and value
+ const listObj =[
+     {
+     name: "Santiago",
+     lastname: "Gallgo"
+ }
+]
+console.log ( listObj.name )
+
+
+ const students =[
+    { name: "Santi",
+    lastname: "Gallgo"},
+    { name: "Jess",
+    lastname: "Orduz"},
+    { name: "Hola",
+    lastname: "Cara de bola"}
+]
+console.log ( students[0].name + " " + students[0].lastname )
+
+//Operators and expresions
+let points = 5
+
+console.log( "Points: " + points)
+
+points = 10
+console.log("Points++: " + points)
+
+
+
